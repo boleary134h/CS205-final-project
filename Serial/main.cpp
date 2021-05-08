38c9200ba5334e62c3581b12381da4ef4812e99e
wuyou309 <56950408+wuyou309@users.noreply.github.com>
2021-05-08 09:18:20 +0800

    Update main.cpp

--- Serial/main.cpp
+++ Serial/main.cpp
@@ -9,60 +9,75 @@ using namespace std;
 
 int main()
 {
+	// To read file names from directory
+	const char *pathPtr = "/home/ubuntu/cnndm_result1";
+	vector<string> fileVec;
+	getFilesFromDir(pathPtr, fileVec);
+	vector<string>::iterator f_iter = fileVec.begin();
 
-	int size[2] = { 0 };
-	int n_doc = 0;
-	string out_filename = "output_0.txt";
+	// Loop over input files
+	// Output saved to current directory
+	string pathname = "/home/ubuntu/cnndm_result1/";
+	for ( ; f_iter != fileVec.end(); f_iter++)
+	{
+		cout << "Processing " << *f_iter << "..." << endl;
+		int size[2] = { 0 };
+		int n_doc = 0;
+		string in_filename = pathname + *f_iter;
+		string out_filename = "output_" + *f_iter;
 
-	ifstream infile;
-	infile.open("0.txt");
-	infile >> n_doc;
+		ifstream infile;
+		infile.open(in_filename);
+		infile >> n_doc;
 
-	// We loop over all documents in current file
-	// Documents are processed one at a time
-	// Output is the indices of the top 10 sentences
-	for (int doc = 0; doc < n_doc; doc++) {
+		// Loop over all documents in current file
+		// Documents are processed one at a time
+		// Output is the indices of the top 3 sentences
+		for (int doc = 0; doc < n_doc; doc++) {
 
-		for (int i = 0; i <= 1; i++) {
-			infile >> size[i];
-		}
-		cout << size[0] << " "<< size[1] << endl;
+			for (int i = 0; i <= 1; i++) {
+				infile >> size[i];
+			}
 
-		// Read vectors representations of sentences in current document
-		vector<vector<double>> sentenceInput;
-		sentenceInput.resize(size[0]);
-		for(int i=0; i<size[0]; i++) {
-			sentenceInput[i].resize(size[1]);
-		}
+			// Read sentence vectors representations
+			vector<vector<double>> sentenceInput;
+			sentenceInput.resize(size[0]);
+			for(int i=0; i<size[0]; i++) {
+				sentenceInput[i].resize(size[1]);
+			}
 
-		for (int i = 0; i < size[0]; i++)
-		{
-			for (int j = 0; j < size[1]; j++)
+			for (int i = 0; i < size[0]; i++)
 			{
-				infile >> sentenceInput[i][j];
+				for (int j = 0; j < size[1]; j++)
+				{
+					infile >> sentenceInput[i][j];
+				}
+			}
+
+			// Instantiate textranker for current document
+			TextRanker textRanker(size[0], size[1]);
+			
+			// Compute similarity matrix
+			textRanker.computeSentenceSimilarity(sentenceInput);
+
+			// Apply Pagerank to get sentence centrality scores
+			textRanker.pageRank();
+
+			// Sort centrality scores
+			textRanker.sortCentrality();
+			
+			// Write indices of top 3 sentences to output
+			// Each line of output file correspond to one document
+			// Output saved to current directory
+			textRanker.writeOutput(3, out_filename);
+
+			for (int m = 0; m < size[0]; m++) {
+				sentenceInput[m].clear();
 			}
 		}
-		cout << sentenceInput[0][0] << ',' << sentenceInput[0][1] << endl;
-		cout << sentenceInput[1][0] << ',' << sentenceInput[1][1] << endl;
+		infile.close();
 
-		// Instantiate textranker for current document
-		// Compute similarity matrix and apply pagerank
-		TextRanker textRanker(size[0], size[1]);
-		textRanker.computeSentenceSimilarity(sentenceInput);
-		textRanker.pageRank();
-
-		// Sort centrality scores and write index of top 10 sentences to output
-		// Each line of output file correspond to one document
-		textRanker.sortCentrality();
-		textRanker.writeOutput(10, out_filename);
-
-		for (int m = 0; m < size[0]; m++) {
-			sentenceInput[m].clear();
-		}
-
-		cout << endl;
 	}
-	infile.close();
 
 	return 0;
 }
